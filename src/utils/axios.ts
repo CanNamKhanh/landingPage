16fca9b10ed2318640a9782b75467cfd3629236a
Can Nam Khanh <khanhcnf8@fullstack.edu.vn>
2026-02-04 05:18:40 +0700

    update

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:3000/";
+const baseURL = "https://rosie-boost-back-end.vercel.app/";
 
 const axiosInstance = axios.create({
   baseURL,
