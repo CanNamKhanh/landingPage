44eb8ed4aea99fe8a7e65d3073fc5335f964a8c4
Can Nam Khanh <khanhcnf8@fullstack.edu.vn>
2026-02-23 12:02:52 +0700

    update chatbox

---
+++ src/components/ChatBox.tsx
@@ -0,0 +1,52 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { MessageSquareMore } from "lucide-react";
+import { useLocation } from "react-router-dom";
+
+function ChatBox() {
+  const location = useLocation();
+  const hiddenChatBoxWhenInMsgPage =
+    location.pathname === "/message" ? "hidden" : "";
+
+  return (
+    <>
+      <Dialog modal={false}>
+        <DialogTrigger
+          asChild
+          className={`w-55 h-12 ${hiddenChatBoxWhenInMsgPage}`}
+        >
+          <button className="fixed bottom-0 right-10 z-50 bg-[#120A21] border-pink-500 border border-b-0 flex items-center gap-3 shadow-[0_0_40px_rgba(0,0,0,0.20)] rounded-t-lg px-4 py-3 cursor-pointer justify-between">
+            <div className="flex gap-3 items-center">
+              <span className="font-medium text-white flex items-center gap-3">
+                <MessageSquareMore />
+                <span>Online</span>
+              </span>
+            </div>
+          </button>
+        </DialogTrigger>
+        <DialogContent
+          onPointerDownOutside={(e) => e.preventDefault()}
+          onInteractOutside={(e) => e.preventDefault()}
+          onEscapeKeyDown={(e) => e.preventDefault()}
+          onPointerDown={(e) => e.stopPropagation()}
+          onClick={(e) => e.stopPropagation()}
+          className="fixed z-50 bottom-0 w-95 translate-x-90 translate-y-0 max-w-none left-1/2 rounded-2xl shadow-2xl transform duration-0 px-0"
+        >
+          <DialogHeader className="">
+            <DialogTitle className="text-[16px] font-medium border-b-2 pb-5 pl-5 flex justify-between">
+              Customer Support
+            </DialogTitle>
+          </DialogHeader>
+          <div className="flex flex-col gap-5 ml-5 -mt-40"></div>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
+}
+
+export default ChatBox;
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,3 +1,4 @@
+import ChatBox from "@/components/ChatBox";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import { Outlet } from "react-router-dom";
@@ -7,6 +8,7 @@ function MainLayout() {
     <div>
       <Header />
       <Outlet />
+      <ChatBox />
       <Footer />
     </div>
   );
